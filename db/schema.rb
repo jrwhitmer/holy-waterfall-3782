fa64c5e5c5405e1c1e7e8e96a86fbbcc939b4e10
Jacq Whitmer <jacqwhitmer@gmail.com>
2021-06-11 10:08:16 -0400

    Additional migrations

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_21_174117) do
+ActiveRecord::Schema.define(version: 2021_06_11_140748) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "airlines", force: :cascade do |t|
     t.string "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "competitions", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.string "sport"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -30,5 +38,38 @@ ActiveRecord::Schema.define(version: 2021_04_21_174117) do
     t.index ["airline_id"], name: "index_flights_on_airline_id"
   end
 
+  create_table "passengers", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+  end
+
+  create_table "players", force: :cascade do |t|
+    t.string "name"
+    t.integer "age"
+    t.bigint "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["team_id"], name: "index_players_on_team_id"
+  end
+
+  create_table "team_competitions", force: :cascade do |t|
+    t.bigint "competition_id"
+    t.bigint "team_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["competition_id"], name: "index_team_competitions_on_competition_id"
+    t.index ["team_id"], name: "index_team_competitions_on_team_id"
+  end
+
+  create_table "teams", force: :cascade do |t|
+    t.string "hometown"
+    t.string "nickname"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   add_foreign_key "flights", "airlines"
+  add_foreign_key "players", "teams"
+  add_foreign_key "team_competitions", "competitions"
+  add_foreign_key "team_competitions", "teams"
 end
